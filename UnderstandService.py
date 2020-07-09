497bdc3464ea44c0f905191efe25362a57881932
Nikesh <nhegde05@gmail.com>
2020-07-09 17:37:41 +0100

    Generated Adjacency MAtrix from csv file Generated Matrix out of Adjanceny Matrix Updated main.py

--- UnderstandService.py
+++ UnderstandService.py
@@ -35,159 +35,162 @@ def get_filenames(db, type):
 """
 This function creates dependencies
 """
-def execute(db,name,path,udb_path):
+def execute(db,name,path,udb_path,type):
     language='java'
     #print('path is '+path)
     #und export -dependencies file csv output.csv myProject.udb
 
-    output = subprocess.check_output("und export -dependencies -format short class matrix class.csv {udb_path}".format(udb_path=udb_path),shell=True)
-    logging.info(output)
-    output = subprocess.check_output("und export -dependencies -format short file matrix file.csv {udb_path}".format(udb_path=udb_path),shell=True)
-    logging.info(output)
 
+    css = """
+    <style type=\"text/css\">
+    table {
+    color: #333;
+    font-family: Helvetica, Arial, sans-serif;
+    width: 640px;
+    border: 1px solid black;
+    }
+    td, th {
+    border: 1px solid transparent; /* No more visible border */
+    height: 30px;
+    }
+    th {
+    background: #DFDFDF; /* Darken header a bit */
+    font-weight: bold;
+    }
+    td {
+    background: #FAFAFA;
+    text-align: center;
+    }
+    table tr:nth-child(odd) td{
+    background-color: white;
+    }
+    </style>
+    """
 
+    script = """
+    <script>
+        let table = document.body.firstElementChild;
 
+        for (let i = 0; i < table.rows.length; i++) {
+          let row = table.rows[i];
+          row.cells[i].style.backgroundColor = 'red';
+        }
+    </script>
+    """
 
+    config = imgkit.config(wkhtmltoimage='D:\\Software\\wkhtmltopdf\\bin\\wkhtmltoimage.exe')
 
-    type='.java'
-    size = len(type)
-    #print('here')
-    #fileset = get_filenames(db,type)
-    #print(fileset)
-    relationship = dict()
-    longnames = []
-    try:
-        for entity in db.ents():
-            if entity.longname()[(-1*size):]==type:
-                relationship[entity.name()] = entity.depends()
-                #longnames.append(entity.longname())
-                #print(entity.filerefs())
-        #print(relationship)
-        #print(len(relationship))
-        headers = list(relationship.keys())
-        #values = list(relationship.values())
-        #print(longnames)
-        print(headers)
-        #print(values)
-        #for d in relationship:
-            #print(relationship.values())
-        #print(len(values))
+    if type.upper()=='FILE':
+        output = subprocess.check_output("und export -dependencies -format short file matrix {path}/file.csv {udb_path}".format(path=path,udb_path=udb_path),shell=True)
+        logging.info(output)
 
-        # list[ key : list[] ]
-
-        #dsm_matrix = [[0 for i in range(len(values))] for j in range(len(values))]
-
-        #for i in range(len(values)):
-        #    for
-
-        y = dict()
-        for key, v  in relationship.items():
-            new_list = []
-            new_list.append(v)
-            #print(key)
-            #print(new_list)
-            #for i in range(len(new_list)):
-                #for k in new_list[i]:
-                    #print('key is ' + key)
-                    #print('relation is ')
-                    #print(k)
-                    #print('value is ')
-                    #print(len(new_list[i][k]))
-                    #y[k] = len(new_list[i][k])
-                    #print(y)
-                    #dsm = dict()
-                    #dsm[key+k] = len(new_list[i][k])
-                #y = dict()
-
-
-        #am = pd.DataFrame(np.zeros(shape=(5,5)), columns=list(headers), index=list(headers))
-        #print(am)
-        #print(path)
-        #am.to_csv(path+'\\file.csv')
-
-
-        df = pd.read_csv('file.csv')
-
-        print(df.head())
-        print(len(df))
-        print(len(df.columns)-1)
+        df = pd.read_csv(path+'/file.csv')
         #print(list(df.columns))
         #print(list(df.index))
         df.fillna(0, inplace=True)
-        df.to_csv('file.csv',index=False,encoding='utf8')
-        #print(df.head())
+        df.to_csv(path+'/file.csv',index=False,encoding='utf8')
+        # #print(df.head())
+        # data = pd.read_csv(open(path+'/file.csv', 'r'),index_col=0)
+        #
+        # text_file = open(path+'/file.html', "a")
+        # # write the CSS
+        # text_file.write(css)
+        # # write the HTML-ized Pandas DataFrame
+        # text_file.write(data.to_html())
+        # text_file.write(script)
+        # text_file.close()
+        #
+        # imgkitoptions = {"format": "png"}
+        # imgkit.from_file(path+'/file.html', path+'/file.png', options=imgkitoptions,config=config)
 
-        df1 = pd.read_csv('class.csv')
 
+
+    if type.upper()=='CLASS':
+        output = subprocess.check_output("und export -dependencies -format short class matrix {path}/class.csv {udb_path}".format(path=path,udb_path=udb_path),shell=True)
+        logging.info(output)
+
+        df1 = pd.read_csv(path+'/class.csv')
         #print(df1.head())
         #print(len(df1))
         #print(len(df1.columns)-1)
         df1.fillna(0, inplace=True)
-        df1.to_csv('class.csv',index=False,encoding='utf8')
+        df1.to_csv(path+'/class.csv',index=False,encoding='utf8')
         #print(df1.head())
-
-        data = pd.read_csv(open("file.csv", "r"),index_col=0)
-        data1 = pd.read_csv(open("class.csv", "r"),index_col=0)
-
-        css = """
-        <style type=\"text/css\">
-        table {
-        color: #333;
-        font-family: Helvetica, Arial, sans-serif;
-        width: 640px;
-        border: 1px solid black;
-        }
-        td, th {
-        border: 1px solid transparent; /* No more visible border */
-        height: 30px;
-        }
-        th {
-        background: #DFDFDF; /* Darken header a bit */
-        font-weight: bold;
-        }
-        td {
-        background: #FAFAFA;
-        text-align: center;
-        }
-        table tr:nth-child(odd) td{
-        background-color: white;
-        }
-        </style>
-        """
-
-        script = """
-        <script>
-            let table = document.body.firstElementChild;
-
-            for (let i = 0; i < table.rows.length; i++) {
-              let row = table.rows[i];
-              row.cells[i].style.backgroundColor = 'red';
-            }
-        </script>
-        """
-
-        config = imgkit.config(wkhtmltoimage='D:\\Software\\wkhtmltopdf\\bin\\wkhtmltoimage.exe')
-
-        text_file = open("file.html", "a")
-        # write the CSS
-        text_file.write(css)
-        # write the HTML-ized Pandas DataFrame
-        text_file.write(data.to_html())
-        text_file.write(script)
-        text_file.close()
-
-        text_file1 = open("class.html", "a")
-        # write the CSS
-        text_file1.write(css)
-        # write the HTML-ized Pandas DataFrame
-        text_file1.write(data1.to_html())
-        text_file1.write(script)
-        text_file1.close()
-
-        imgkitoptions = {"format": "png"}
-        imgkit.from_file("file.html", "file.png", options=imgkitoptions,config=config)
-        imgkit.from_file("class.html", "class.png", options=imgkitoptions,config=config)
+        #data1 = pd.read_csv(open(path+'/class.csv', 'r'),index_col=0)
 
 
-    except understand.UnderstandError as e:
-        print(e)
+
+        # text_file1 = open(path+'/class.html', "a")
+        # # write the CSS
+        # text_file1.write(css)
+        # # write the HTML-ized Pandas DataFrame
+        # text_file1.write(data1.to_html())
+        # text_file1.write(script)
+        # text_file1.close()
+        #
+        # imgkitoptions = {"format": "png"}
+        # imgkit.from_file(path+'/class.html',path+'/class.png', options=imgkitoptions,config=config)
+
+
+
+    # type='.java'
+    # size = len(type)
+    # #print('here')
+    # #fileset = get_filenames(db,type)
+    # #print(fileset)
+    # relationship = dict()
+    # longnames = []
+    # try:
+    #     for entity in db.ents():
+    #         if entity.longname()[(-1*size):]==type:
+    #             relationship[entity.name()] = entity.depends()
+    #             #longnames.append(entity.longname())
+    #             #print(entity.filerefs())
+    #     #print(relationship)
+    #     #print(len(relationship))
+    #     headers = list(relationship.keys())
+    #     #values = list(relationship.values())
+    #     #print(longnames)
+    #     print(headers)
+    #     #print(values)
+    #     #for d in relationship:
+    #         #print(relationship.values())
+    #     #print(len(values))
+    #
+    #     # list[ key : list[] ]
+    #
+    #     #dsm_matrix = [[0 for i in range(len(values))] for j in range(len(values))]
+    #
+    #     #for i in range(len(values)):
+    #     #    for
+    #
+    #     y = dict()
+    #     for key, v  in relationship.items():
+    #         new_list = []
+    #         new_list.append(v)
+    #         #print(key)
+    #         #print(new_list)
+    #         #for i in range(len(new_list)):
+    #             #for k in new_list[i]:
+    #                 #print('key is ' + key)
+    #                 #print('relation is ')
+    #                 #print(k)
+    #                 #print('value is ')
+    #                 #print(len(new_list[i][k]))
+    #                 #y[k] = len(new_list[i][k])
+    #                 #print(y)
+    #                 #dsm = dict()
+    #                 #dsm[key+k] = len(new_list[i][k])
+    #             #y = dict()
+    #
+    #
+    #     #am = pd.DataFrame(np.zeros(shape=(5,5)), columns=list(headers), index=list(headers))
+    #     #print(am)
+    #     #print(path)
+    #     #am.to_csv(path+'\\file.csv')
+    #
+    #
+    #
+    #
+    # except understand.UnderstandError as e:
+    #     print(e)
--- main.py
+++ main.py
@@ -12,14 +12,19 @@ from github import Github
 import configparser
 import os
 import shutil
+import json
+import pandas as pd
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
 
-repo_name='nikeshhegde/RoundRobinDataCentre'
-proj_name = 'nikeshhegde/RoundRobinDataCentre'
+#repo_name='nikeshhegde/RoundRobinDataCentre'
+#proj_name = 'nikeshhegde/RoundRobinDataCentre'
 project_path='D:\\code'
 
 
 #github Authentication using token
-headers={'Authorization':'token fae07fdf65253e14cb1f2f80235fd11ebb599e4c',
+headers = {'Authorization':'token 6c03934cde39dde8d6050584ca9e9011d8b42c85',
         'User-Agent':'https://api.github.com/meta',
         'Content-Type':'application/json'}
 #get the fulll project name of the
@@ -33,7 +38,8 @@ def get_repo_url(pull_req): # Get the repo URL to clone the repo
         if pull_req.status_code == 200:
                 pull2=pull_req.json()
                 repo_url=pull2['items'][0]['html_url']
-                print('Repo_URL:',repo_url)
+                #repo_url=pull2[]['html_url']
+                #print('Repo_URL:',repo_url)
                 #response=requests.get(repo_url,headers=headers)
                 #if pull_req.status_code == 200:
                     #repo_url=response.json()['html_url']
@@ -43,14 +49,14 @@ def get_repo_url(pull_req): # Get the repo URL to clone the repo
 
     return repo_url
 
-def get_repo_sha(pull_req)    : # Get the repo URL to clone the repo
+def get_repo_sha(pull_req)    : # Get the repo SHA1 to clone the repo
     sha=''
 
     try:
         if pull_req.status_code == 200:
                 pull_sha=pull_req.json()
                 sha=pull_sha[0]['sha']
-                print('SHA1:',sha)
+                #print('SHA1:',sha)
                 #response=requests.get(repo_url,headers=headers)
                 #if pull_req.status_code == 200:
                     #repo_url=response.json()['html_url']
@@ -60,11 +66,11 @@ def get_repo_sha(pull_req)    : # Get the repo URL to clone the repo
 
     return sha
 
-def clone_repo(repo_url,name):
+def clone_repo(repo_url,name,num):
     try:
         git_url=repo_url
         print('git_url is' ,git_url)
-        repo_dir=project_path +'\\'+ name
+        repo_dir=project_path +'\\'+ name+'\\'+str(num)
         if os.path.isdir(repo_dir):
             os.system('rmdir /S /Q "{}"'.format(repo_dir))
 
@@ -78,11 +84,11 @@ def clone_repo(repo_url,name):
 
 
 #Checkout the version of the project using the SHA for that version
-def git_checkout(sha,projectname):
+def git_checkout(sha,projectname,num):
 
         result = []
         flag=True
-        path = project_path+'\\'+projectname
+        path = project_path+'\\'+projectname+'\\'+str(num)+'\\'
         try:
             os.mkdir(path)
         except OSError:
@@ -110,52 +116,16 @@ def start():
 
         proj_name = 'nikeshhegde/RoundRobinDataCentre'
         #search = 'https://api.github.com/search/repositories?q=repo:yankils/Simple-DevOps-Project'
-        #search_req = requests.get('https://api.github.com/search/repositories?q=repo:'+proj_name,headers=headers)
-
+        search_req = requests.get('https://api.github.com/search/repositories?q=repo:'+proj_name,headers=headers)
+        #print(search_req.json())
         projectname=proj_name.split("/")[1]
         print('Project name:',projectname)
 
         # get the repository URL
-        #repo_url=get_repo_url(search_req)
+        repo_url=get_repo_url(search_req)
 
-        #Clone the repository
-        #repo_dir1=clone_repo(repo_url,projectname)
-
-        #pull_req = requests.get('https://api.github.com/repos/'+proj_name+'/commits?since='+since_datetime+'&until='+until_datetime,headers=headers)
-
-        # get the repository URL
-        #repo_sha=get_repo_sha(search_req)
-        #print(repo_url)
-
-        #Clone the repository
-        #flag = git_checkout(repo_sha,projectname)
-        #print(flag)
-        repo_dir1='D:\\code\\RoundRobinDataCentre'
-
-        udb_path=repo_dir1
-        project_root=repo_dir1
-        language='java'
-
-        print('path is' + r''+udb_path)
-
-        #create_udb(r''+udb_path+'\\'+projectname, language, project_root)
-        path1=r''+udb_path+'\\'+projectname+'.udb'
-        print('udb path is' + path1)
-
-        try:
-            db1 = understand.open(path1)
-
-        except understand.UnderstandError as e:
-            logging.fatal('udb open failed')
-            raise Exception
-
-        print('before executing.....')
-        #Executing Understand analysis
-        execute(db1,projectname,repo_dir1,path1)
-
-        db1.close()
-
-
+        #call github service
+        test_github_service(repo_url)
 
 
     except understand.UnderstandError as e:
@@ -163,5 +133,180 @@ def start():
         return 0
         return np
 
+def test_github_service(repo_url):
+    print("inside test_github_service")
+    repo_name='nikeshhegde/RoundRobinDataCentre'
+    since_datetime = '2019-11-29T22:16:53Z'
+    until_datetime = '2020-04-20T22:16:53Z'
+    request = 'https://api.github.com/repos/'+repo_name+'/commits?since='+since_datetime+'&until='+until_datetime
+    print(request)
+    commit_date = []
+    git_URL = []
+    sha = []
+    search_req = requests.get(request,headers=headers)
+    if search_req.status_code == 200:
+        print("inside 200")
+        json_data = search_req.json()
+        for data in json_data:
+            commit_date.append(data['commit']['author']['date'])
+            git_URL.append(data['html_url'])
+            sha.append(data['sha'])
+    print(len(sha))
+    #print(git_URL[0].rpartition('/commits'))
+    projectname=repo_name.split("/")[1]
+    print('Project name:',projectname)
+    code_versions(repo_url,sha,projectname)
+
+# function to checkout versions of the code by particular sha
+def code_versions(repo_url,sha,projectname):
+    for i in range(len(sha)):
+        #Clone the repository
+        repo_dir=clone_repo(repo_url,projectname,i)
+        print("repo_dir " +repo_dir)
+
+        #Clone the repository
+        flag = git_checkout(sha[i],projectname,i)
+        print(flag)
+
+        udb_path=repo_dir
+        project_root=repo_dir
+        language='java'
+        udb_path= udb_path+'\\'+projectname
+        print('path is ' + r''+udb_path)
+
+        create_udb(r''+udb_path, language, project_root)
+        path=r''+udb_path+'.udb'
+        print('udb path is' + path)
+
+
+        try:
+            db = understand.open(path)
+
+        except understand.UnderstandError as e:
+            logging.fatal('udb open failed')
+            raise Exception
+
+        print('before executing.....')
+        # get the type of dependencies required
+        type = 'CLASS'
+        #Executing Understand analysis
+        execute(db,projectname,repo_dir,path,type)
+
+        db.close()
+
+# stores the vertices in the graph
+vertices = []
+graph = []
+vertices_no = 0
+# Add a vertex to the set of vertices and the graph
+def add_vertex(v):
+  global graph
+  global vertices_no
+  global vertices
+  if v in vertices:
+    print("Vertex ", v, " already exists")
+  else:
+    vertices_no = vertices_no + 1
+    vertices.append(v)
+    if vertices_no > 1:
+        for vertex in graph:
+            vertex.append(0)
+    temp = []
+    for i in range(vertices_no):
+        temp.append(0.0)
+    graph.append(temp)
+
+# Add an edge between vertex v1 and v2 with edge weight e
+def add_edge(v1, v2, e):
+    global graph
+    global vertices_no
+    global vertices
+    # Check if vertex v1 is a valid vertex
+    if v1 not in vertices:
+        print("Vertex ", v1, " does not exist.")
+    # Check if vertex v1 is a valid vertex
+    elif v2 not in vertices:
+        print("Vertex ", v2, " does not exist.")
+    # Since this code is not restricted to a directed or
+    # an undirected graph, an edge between v1 v2 does not
+    # imply that an edge exists between v2 and v1
+    else:
+        index1 = vertices.index(v1)
+        index2 = vertices.index(v2)
+        graph[index1][index2] = e
+
+
+def get_differences(sha):
+
+    #total_dir = len(sha)
+    for i in range(sha):
+        #print(i)
+        if i < sha:
+            file_path = 'D:\\code\\RoundRobinDataCentre\\'
+            file_path = file_path+str(i)
+            #print(file_path)
+            #file_path2 = file_path+str(i+1)
+            file1 = file_path+'\\class.csv'
+            #file2 = file_path2+'\\class.csv'
+            #print(file1)
+            #print(file2)
+            #print('----------')
+            #f1 = pd.read_csv(file1)
+            #f2 = pd.read_csv(file2)
+            #file_path = 'D:\\code\\RoundRobinDataCentre\\0\\class.csv'
+            #file_path1 = 'D:\\code\\RoundRobinDataCentre\\1\\class.csv'
+            with open(file1,'rt') as f:
+                #fileObject = csv.reader(f)
+                #row_count = sum(1 for row in fileObject)
+                #print(row_count)
+                data = csv.reader(f)
+                headers = list(next(data))  # gets the first line
+                for i in range(len(headers)):
+                    if i > 0:
+                        #vertices.append(headers[i])
+                        add_vertex(headers[i])
+                        #print(vertices)
+                #print(vertices_no)
+                # sort the graph
+                #vertices.sort()
+                #print(vertices)
+                #print(graph)
+                for row in data:
+                    values = list(row)
+                    #print(values)
+                    for i in range(len(values)):
+                        if i == 0:
+                            vertex_name = values[0]
+                        else:
+                            #print(i)
+                            add_edge(vertex_name,vertices[i-1],float(values[i]))
+                #print(graph)
+                # Display matrix
+                fig, ax = plt.subplots()
+                im = ax.imshow(graph)
+                ax.set_xticks(np.arange(len(vertices)))
+                ax.set_yticks(np.arange(len(vertices)))
+                ax.set_xticklabels(vertices)
+                ax.set_yticklabels(vertices)
+                ax.xaxis.tick_top()
+                # Loop over data dimensions and create text annotations.
+                for i in range(len(vertices)):
+                    for j in range(len(vertices)):
+                        text = ax.text(j, i, graph[i][j],
+                                       ha="center", va="center", color="w")
+                # Rotate the tick labels and set their alignment.
+                plt.setp(ax.get_xticklabels(), rotation=45, ha="left",
+                         rotation_mode="anchor")
+                fig.tight_layout()
+                #file_path = 'D:\\code\\RoundRobinDataCentre\\0\\'
+                #if not os.path.isdir(results_dir):
+                    #os.makedirs(results_dir)
+                #pdf.savefig(fig)
+                #print(file_path)
+                plt.savefig(file_path +'/imageclass.png')
+                #plt.show()
+                #plt.matshow(graph,0)
+                #plt.show()
 if __name__ == '__main__':
-    start()
+    #start()
+    get_differences(8)
